5f9d7923a1a01cabab39a2029d05b81978f3c702
Nahuel Soma <soma.nahuel@gmail.com>
2022-04-13 15:52:07 -0300

    Se crea el script con el llamado a la API y se vincula con el script de la Home.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,23 @@
-const Home = () => {
+import getData from "../utils/getData";
+
+const Home = async () => {
+  const characters = await getData();
   const view = `
     <div class="Characters">
+    ${characters.results
+      .map(
+        (character) => `
         <article class="Character-item">
-            <a href="#/1/>
-                <img src="image" alt="name">
-                <h2>Name</h2>
-            </a>
+          <a href="#/${character.id}/">
+            <img src="${character.image}" alt="${character.name}">
+            <h2>${character.name}</h2>
+          </a>
         </article>
+      `
+      )
+      .join("")}
     </div>
-    `;
+  `;
   return view;
 };
 
